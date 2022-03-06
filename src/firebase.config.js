b7249859a9420384b783f3bce2d24674800f7c33
Waleed Saifullah <waleed.saifullah@axiomworld.net>
2022-03-06 13:31:42 +0500

    [Create Listings]

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDXaGAttH8Wip3r7PYB9giNUjPAq02ULmQ",
-  authDomain: "house-marketplace-app-143b5.firebaseapp.com",
-  projectId: "house-marketplace-app-143b5",
-  storageBucket: "house-marketplace-app-143b5.appspot.com",
-  messagingSenderId: "390646899179",
-  appId: "1:390646899179:web:92d35f0199c13b6ab2b9ae"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
--- src/pages/CreateListing.js
+++ src/pages/CreateListing.js
@@ -1,6 +1,23 @@
 import React, { useState, useEffect, useRef } from 'react'
+
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import {
+    getStorage,
+    ref,
+    uploadBytesResumable,
+    getDownloadURL,
+} from 'firebase/storage'
+import {
+    addDoc,
+    collection,
+    serverTimestamp 
+} from 'firebase/firestore'
+import { db } from '../firebase.config'
+
 import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { v4 as uuidv4 } from 'uuid'
+
 import Spinner from '../components/Spinner'
 
 const CreateListing = () => {
@@ -58,35 +75,131 @@ const CreateListing = () => {
         }
     }, [isMounted])
 
-    const onSubmit = (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault()
-        console.log(formData)
+        // console.log(formData)
+
+        setLoading(true)
+
+        if (discountedPrice >= regularPrice) {
+            setLoading(false)
+            toast.error('Discounted price needs to be less than regular price')
+            return
+        }
+
+        if (images.length > 6) {
+            setLoading(false)
+            toast.error('Max 6 images')
+            return
+        }
+
+        let geolocation = {}
+        let location
+
+        if (geolocationEnabled) {
+
+        } else {
+            geolocation.lat = latitude
+            geolocation.lng = longitude
+            // location = address
+        }
+
+        // Store images in firebase
+        const storeImage = async (image) => {
+            return new Promise((resolve, reject) => {
+                const storage = getStorage()
+                const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+
+                const storageRef = ref(storage, 'images/' + fileName)
+
+                const uploadTask = uploadBytesResumable(storageRef, image)
+
+                uploadTask.on(
+                    'state_changed',
+                    (snapshot) => {
+                        const progress =
+                            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                        console.log('Upload is ' + progress + '% done')
+                        switch (snapshot.state) {
+                            case 'paused':
+                                console.log('Upload is paused')
+                                break
+                            case 'running':
+                                console.log('Upload is running')
+                                break
+                            default:
+                                break
+                        }
+                    },
+                    (error) => {
+                        reject(error)
+                    },
+                    () => {
+                        // Handle successful uploads on complete
+                        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                            resolve(downloadURL)
+                        })
+                    }
+                )
+
+            })
+        }
+
+        const imgUrls = await Promise.all(
+                [...images].map((image) => storeImage(image))
+            ).catch(() => {
+                setLoading(false)
+                toast.error('Images not uploaded')
+                return
+            }
+        )
+
+        const formDataCopy = {
+            ...formData,
+            imgUrls,
+            geolocation,
+            timestamp: serverTimestamp()
+        }
+
+        formDataCopy.location = address
+        delete formDataCopy.images
+        delete formDataCopy.address
+        !formDataCopy.offer && delete formDataCopy.discountedPrice
+
+        const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
+            setLoading(false)
+            toast.success('Listing saved')
+            navigate(`/category/${formDataCopy.type}/${docRef.id}`
+        )
+
+        // setLoading(false)
     }
 
     const onMutate = (e) => {
         let boolean = null
 
         if (e.target.value === 'true') {
-          boolean = true
+            boolean = true
         }
         if (e.target.value === 'false') {
-          boolean = false
+            boolean = false
         }
-    
+
         // Files
         if (e.target.files) {
-          setFormData((prevState) => ({
-            ...prevState,
-            images: e.target.files,
-          }))
+            setFormData((prevState) => ({
+                ...prevState,
+                images: e.target.files,
+            }))
         }
-    
+
         // Text/Booleans/Numbers
         if (!e.target.files) {
-          setFormData((prevState) => ({
-            ...prevState,
-            [e.target.id]: boolean ?? e.target.value,
-          }))
+            setFormData((prevState) => ({
+                ...prevState,
+                [e.target.id]: boolean ?? e.target.value,
+            }))
         }
     }
 
